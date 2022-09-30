4059b7d0157d54fc4ce3348b87a52b6934ecb0a9
Elena Petrova <elena.i.11@yandex.ru>
2022-09-30 13:04:09 +0400

    task4

--- sem2/task2-4/task2-4.py
+++ sem2/task2-4/task2-4.py
@@ -1,6 +1,18 @@
 #Реализуйте алгоритм перемешивания списка.
 
-import random
-listA = [1, 6, 7, 3, 9]
-random.shuffle (listA)
-print (listA)
+number = int(input("Размер списка: "))
+
+listA = []
+
+for i in range(number):
+    listA.append(i)
+
+print(listA)
+
+list_shuffled = []
+
+for i in range(0, len(list)):
+    j = random.randomrange(0, len(number))
+    list_shuffled.append(number[j])
+    list.remowe(number[j])
+print(list_shuffled)
